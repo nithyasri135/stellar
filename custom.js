a3eb00219952396415d647f677e476ef3d2ded83
nithyasri135 <150986936+nithyasri135@users.noreply.github.com>
2025-03-25 22:18:22 +0530

    Delete custom.js

--- custom.js
+++
@@ -1,8 +0,0 @@
-function nithi(){
-    document.getElementById("customAlert").style.display="block";
-
-}
-function closeCustomAlert(){
-    document.getElementById("customAlert").style.display="none";
-    
-}
